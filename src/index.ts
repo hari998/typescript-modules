b1234aaaf1ac558839cdbf826713f7984f1c5023
hari <heyhariom24@gmail.com>
2023-05-19 15:41:54 +0530

    commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { add } from "./add.js";
+import { subtract } from "./subtract.js";
+
+export { add, subtract }
+
+/**
+ * This means that a user can get at our functions by 
+ * importing just what they need, or by getting everything:
+ * 
+ * import { add } from 'maths-package';
+ * import * as MathsPackage from 'maths-package';
+ */
